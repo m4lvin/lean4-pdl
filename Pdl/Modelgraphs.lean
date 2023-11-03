8d4eeb4676a803923888a19ac93f4431664aec4e
Malvin Gattinger <malvin@w4eg.de>
2023-11-03 14:47:51 +0100

    Manfred was lazy in Lemma 9

--- Pdl/Modelgraphs.lean
+++ Pdl/Modelgraphs.lean
@@ -6,98 +6,127 @@ open Formula
 
 -- Definition 18, page 31
 def Saturated : Finset Formula → Prop
-  | X => ∀ P Q : Formula, ((~~P) ∈ X → P ∈ X)
-                        ∧ (P⋀Q ∈ X → P ∈ X ∧ Q ∈ X)
-                        ∧ ((~(P⋀Q)) ∈ X → (~P) ∈ X ∨ (~Q) ∈ X)
-                        -- TODO: closure for program combinators!
+  | X => ∀ (P Q : Formula) (a b : Program),
+    -- propositional closure:
+      ((~~P) ∈ X → P ∈ X)
+    ∧ (P⋀Q ∈ X → P ∈ X ∧ Q ∈ X)
+    ∧ ((~(P⋀Q)) ∈ X → (~P) ∈ X ∨ (~Q) ∈ X)
+    -- programs closure:
+    ∧ ((⌈a;'b⌉P) ∈ X → (⌈a⌉⌈b⌉P) ∈ X)
+    ∧ ((⌈a⋓b⌉P) ∈ X → (⌈a⌉P) ∈ X ∧ (⌈b⌉P) ∈ X)
+    ∧ ((⌈?'Q⌉P) ∈ X → ¬Q ∈ X ∨ P ∈ X)
+    ∧ ((⌈∗a⌉P) ∈ X → P ∈ X ∧ (⌈a⌉⌈∗a⌉P) ∈ X)
+    ∧ ((~⌈a;'b⌉P) ∈ X → (~⌈a⌉⌈b⌉P) ∈ X)
+    ∧ ((~⌈a⋓b⌉P) ∈ X  → (~⌈a⌉P) ∈ X ∨ (~⌈b⌉P) ∈ X)
+    ∧ ((~⌈?'Q⌉P) ∈ X → Q ∈ X ∧ (~P) ∈ X)
+    ∧ ((~⌈∗a⌉P) ∈ X → (~P) ∈ X ∨ (~⌈a⌉⌈∗a⌉P) ∈ X)
 
 -- Definition 19, page 31
--- TODO: change [] to [A] later
-@[simp]
 def ModelGraph (W : Finset (Finset Formula)) :=
   let i := ∀ X : W, Saturated X.val ∧ ⊥ ∉ X.val ∧ ∀ P, P ∈ X.val → (~P) ∉ X.val
-  let ii := fun M : KripkeModel W => ∀ (X : W) p, (·p : Formula) ∈ X.val ↔ M.val X p
-  let iii :=-- Note: Borzechowski only has → in ii. We follow BRV, Def 4.18 and 4.84.
-  fun M : KripkeModel W => ∀ (X Y : W) A P, M.Rel A X Y → (⌈·A⌉P) ∈ X.val → P ∈ Y.val
-  let iv := fun M : KripkeModel W => ∀ (X : W) A P, (~⌈·A⌉P) ∈ X.val → ∃ Y, M.Rel A X Y ∧ (~P) ∈ Y.val
+  let ii M := ∀ (X : W) p, (·p : Formula) ∈ X.val ↔ M.val X p
+  -- Note: Borzechowski only has → in ii. We follow BRV, Def 4.18 and 4.84.
+  let iii M := ∀ (X Y : W) A P, M.Rel A X Y → (⌈·A⌉P) ∈ X.val → P ∈ Y.val
+  let iv M := ∀ (X : W) A P, (~⌈·A⌉P) ∈ X.val → ∃ Y, M.Rel A X Y ∧ (~P) ∈ Y.val
   Subtype fun M : KripkeModel W => i ∧ ii M ∧ iii M ∧ iv M
 
+-- Lemma 9, page 32, stronger version for induction loading.
+theorem loadedTruthLemma {Worlds} (MG : ModelGraph Worlds) X:
+    ∀ P,     (P ∈ X.val → evaluate MG.val X P) -- (+)
+        ∧ ((~P) ∈ X.val → ¬evaluate MG.val X P) -- (-)
+        ∧ ∀ a, ((⌈a⌉P) ∈ X.val → (∀ (Y : Worlds), relate MG.val a X Y → P ∈ Y.val)) -- (0)
+    :=
+  by
+  intro P
+  cases P -- no "induction", use recursive calls!
+  case bottom =>
+    repeat' constructor
+    · intro P_in_X
+      apply absurd P_in_X
+      have ⟨M,⟨i,_,_,_⟩⟩ := MG
+      specialize i X
+      tauto
+    · simp
+    · intro a
+      -- TODO cases a
+      all_goals simp
+      intro boxBot_in_X Y X_rel_Y
+      have ⟨M,⟨_,_,iii,_⟩⟩ := MG
+      sorry -- exact iii X Y _ _ _ boxBot_in_X
+  case atom_prop pp =>
+    repeat' constructor
+    · intro P_in_X; unfold evaluate; sorry -- rw [ii X pp] at P_in_X ; exact P_in_X
+    · intro notP_in_X; unfold evaluate; sorry /- rw [← ii X pp]
+      rcases i X with ⟨_, _, P_in_then_notP_not_in⟩
+      specialize P_in_then_notP_not_in (·pp); tauto -/
+    · intro boxPp_in_X Y X_rel_Y; sorry -- exact iii X Y A (·pp) X_rel_Y boxPp_in_X
+  case neg Q => -- what is the Y here?
+    have IH := loadedTruthLemma MG X Q
+    have ⟨plus,minus,oh⟩ := IH
+    repeat' constructor
+    · intro notQ_in_X
+      simp
+      exact minus notQ_in_X
+    · intro notnotQ_in_X
+      simp
+      have ⟨M,⟨i,_,_,_⟩⟩ := MG
+      rcases i X with ⟨X_saturated, _, _⟩
+      exact plus ((X_saturated Q Q (?'Q) (?'Q)).left notnotQ_in_X)
+    · intro notQ_in_X Y X_rel_Y
+      have ⟨M,⟨_,_,iii,_⟩⟩ := MG
+      sorry -- exact iii X Y A (~Q) X_rel_Y notQ_in_X
+  case and Q R =>
+    have ⟨plus_Q, minus_Q, oh_Q⟩ := loadedTruthLemma MG X Q
+    have ⟨plus_R, minus_R, oh_R⟩ := loadedTruthLemma MG X R
+    have ⟨M,⟨i,_,iii,_⟩⟩ := MG
+    rcases i X with ⟨X_saturated, _, _⟩
+    unfold Saturated at X_saturated
+    rcases X_saturated Q R (?'Q) (?'Q) with ⟨_, ⟨andSplit, ⟨notAndSplit, _⟩⟩⟩
+    repeat' constructor
+    · intro QandR_in_X
+      specialize andSplit QandR_in_X
+      cases' andSplit with Q_in_X R_in_X
+      simp
+      constructor
+      · exact plus_Q Q_in_X
+      · exact plus_R R_in_X
+    · intro notQandR_in_X
+      unfold evaluate; rw [not_and_or]
+      specialize notAndSplit notQandR_in_X
+      cases' notAndSplit with notQ_in_X notR_in_X
+      · left; exact minus_Q notQ_in_X
+      · right; exact minus_R notR_in_X
+    · intro QandR_in_X Y X_rel_Y
+      sorry -- exact iii X Y A (Q⋀R) X_rel_Y QandR_in_X
+  case box a P =>
+    repeat' constructor
+    all_goals simp
+    · intro boxP_in_X
+      intro Y Y_in X_rel_Y
+      have ⟨plus_Y, _, _⟩ := loadedTruthLemma MG ⟨Y, Y_in⟩ P
+      have ⟨_, _, oh_X⟩ := loadedTruthLemma MG X P
+      apply plus_Y
+      apply oh_X a boxP_in_X ⟨Y,Y_in⟩ X_rel_Y
+    · intro notBoxP_in_X
+      have ⟨M,⟨i,ii,iii,iv⟩⟩ := MG
+      cases a
+      case atom_prog A =>
+        rcases iv X A P notBoxP_in_X with ⟨Y, X_rel_Y, notP_in_Y⟩
+        have ⟨_, minus_Y, _⟩ := loadedTruthLemma ⟨M,⟨i,ii,iii,iv⟩⟩ Y P
+        use Y
+        simp
+        constructor
+        exact X_rel_Y
+        exact minus_Y notP_in_Y
+      all_goals sorry
+    · intro b boxBoxP_in_X Y y_in X_rel_Y
+      have ⟨M,⟨_, _, iii, _⟩⟩ := MG
+      sorry -- exact iii X Y b (⌈a⌉P) X_rel_Y boxBoxP_in_X
+
 -- Lemma 9, page 32
-theorem truthLemma {Worlds : Finset (Finset Formula)} (MG : ModelGraph Worlds) :
+theorem truthLemma {Worlds} (MG : ModelGraph Worlds) :
     ∀ X : Worlds, ∀ P, P ∈ X.val → evaluate MG.val X P :=
   by
   intro X P
-  cases' MG with M M_prop
-  rcases M_prop with ⟨i, ii, iii, iv⟩
-  -- induction loading!!
-  let plus P (X : Worlds) := P ∈ X.val → evaluate M X P
-  let minus P (X : Worlds) := (~P) ∈ X.val → ¬evaluate M X P
-  let oh A P (X : Worlds) := (⌈·A⌉P) ∈ X.val → ∀ Y, M.Rel A X Y → P ∈ Y.val
-  have claim : ∀ A P X, plus P X ∧ minus P X ∧ oh A P X :=
-    by
-    intro A P
-    cases P -- no "induction", use recursive calls!
-    all_goals intro X
-    case bottom =>
-      specialize i X
-      rcases i with ⟨_, bot_not_in_X, _⟩
-      repeat' constructor
-      · intro P_in_X; apply absurd P_in_X bot_not_in_X
-      · tauto
-      · intro boxBot_in_X Y X_rel_Y; exact iii X Y A ⊥ X_rel_Y boxBot_in_X
-    case atom_prop pp =>
-      repeat' constructor
-      · intro P_in_X; unfold evaluate; rw [ii X pp] at P_in_X ; exact P_in_X
-      · intro notP_in_X; unfold evaluate; rw [← ii X pp]
-        rcases i X with ⟨_, _, P_in_then_notP_not_in⟩
-        specialize P_in_then_notP_not_in (·pp); tauto
-      · intro boxPp_in_X Y X_rel_Y; exact iii X Y A (·pp) X_rel_Y boxPp_in_X
-    case neg P IH =>
-      rcases IH X with ⟨plus_IH, minus_IH, _⟩
-      repeat' constructor
-      · intro notP_in_X; unfold evaluate
-        exact minus_IH notP_in_X
-      · intro notnotP_in_X
-        rcases i X with ⟨X_saturated, _, _⟩
-        cases' X_saturated P ⊥ with doubleNeg
-        -- ⊥ is unused!
-        unfold evaluate;
-        simp
-        exact plus_IH (doubleNeg notnotP_in_X)
-      · intro notPp_in_X Y X_rel_Y; exact iii X Y A (~P) X_rel_Y notPp_in_X
-    case and P Q IH_P IH_Q =>
-      rcases IH_P X with ⟨plus_IH_P, minus_IH_P, _⟩
-      rcases IH_Q X with ⟨plus_IH_Q, minus_IH_Q, _⟩
-      rcases i X with ⟨X_saturated, _, _⟩
-      unfold Saturated at X_saturated
-      rcases X_saturated P Q with ⟨_, andSplit, notAndSplit⟩
-      repeat' constructor
-      · intro PandQ_in_X
-        specialize andSplit PandQ_in_X; cases' andSplit with P_in_X Q_in_X
-        unfold evaluate
-        constructor
-        · exact plus_IH_P P_in_X
-        · exact plus_IH_Q Q_in_X
-      · intro notPandQ_in_X
-        unfold evaluate; rw [not_and_or]
-        specialize notAndSplit notPandQ_in_X
-        cases' notAndSplit with notP_in_X notQ_in_X
-        · left; exact minus_IH_P notP_in_X
-        · right; exact minus_IH_Q notQ_in_X
-      · intro PandQ_in_X Y X_rel_Y; exact iii X Y A (P⋀Q) X_rel_Y PandQ_in_X
-    case box B A P =>
-      repeat' constructor
-      · intro boxP_in_X; unfold evaluate
-        intro Y X_rel_Y
-        rcases IH Y with ⟨plus_IH_Y, _, _⟩
-        apply plus_IH_Y
-        rcases IH X with ⟨_, _, oh_IH_X⟩
-        exact oh_IH_X boxP_in_X Y X_rel_Y
-      · intro notBoxP_in_X; unfold Evaluate
-        push_neg
-        rcases iv X P notBoxP_in_X with ⟨Y, X_rel_Y, notP_in_Y⟩
-        use Y; constructor; exact X_rel_Y
-        rcases IH Y with ⟨_, minus_IH_Y, _⟩
-        exact minus_IH_Y notP_in_Y
-      · intro boxBoxP_in_X Y X_rel_Y; exact iii X Y A (⌈·B⌉P) X_rel_Y boxBoxP_in_X
-  apply (claim A P X).left
+  have := loadedTruthLemma MG X P
+  tauto
