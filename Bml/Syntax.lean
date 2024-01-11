c1968cfff806cf6d862a003266b77f873f90deef
W-Bosman <73535959+W-Bosman@users.noreply.github.com>
2024-01-11 14:11:35 +0100

    Changed Tableau children to Lists
    Changed Tableau children to Lists in order to determinize interpolant construction.
    
    Slight changes to InterpolantInductionStep since Lean doesn't understand the type 'myList -> Formula' so now we use a Pi type

--- Bml/Syntax.lean
+++ Bml/Syntax.lean
@@ -40,11 +40,11 @@ instance : Top Formula :=
   ⟨Formula.neg Formula.bottom⟩
 
 -- Convienient constructors
-noncomputable def BigConjunction : Finset Formula → Formula := λX =>
-  List.foldl Formula.And (~Formula.bottom) (Finset.toList X)
+def BigConjunction : List Formula → Formula :=
+  List.foldl Formula.And (~Formula.bottom)
 
-noncomputable def BigDisjunction : Finset Formula → Formula := λX =>
-  List.foldl (λϕ ψ => ~(~ϕ⋀~ψ)) (Formula.bottom) (Finset.toList X)
+def BigDisjunction : List Formula → Formula :=
+  List.foldl (λϕ ψ => ~(~ϕ⋀~ψ)) (Formula.bottom)
 
 -- showing formulas as strings that are valid Lean code
 def formToString : Formula → ℕ → Lean.Format
