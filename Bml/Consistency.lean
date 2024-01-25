45423fff192c219d2a338e99a78a4b4e0ec11cbf
18jesorkin <18jesorkin@gmail.com>
2024-01-25 17:02:22 -0500

    Beginning conThenConSucc lemma

--- Bml/Consistency.lean
+++ Bml/Consistency.lean
@@ -20,7 +20,11 @@ open LocalRule
 
 
 
+theorem InconsisIffNotConsis : Inconsistent X ↔ ¬ Consistent X := by
+  refine iff_def.mpr ?_; refine And.intro ?_ ?_;
+  intro inconsisX consisX; unfold Consistent at *; unfold Inconsistent at *; exact consisX inconsisX
 
+  intro NotconsisX; unfold Consistent at NotconsisX; simp at NotconsisX; exact NotconsisX
 
 
 -- Obvious consequence of Consistency
@@ -64,6 +68,33 @@ theorem consThenConEndnode : Consistent X → ∀ tX : LocalTableau X, ∃ E ∈
   have closTabX : ClosedTableau X := ClosedTableau.loc LocTabX h₀
   unfold Consistent Inconsistent at *; simp at *; exact IsEmpty.false closTabX
 
+-- Every endnode Inconsistent means root is Inconsistent
+theorem InconsThenInconsEndnode : (∃ tX : LocalTableau X, ∀ E ∈ endNodesOf ⟨X, tX⟩, Inconsistent E) → Inconsistent X := by
+  intro h₀; rcases h₀ with ⟨LocTabX, endNodeIncon⟩
+  by_contra consisX; unfold Inconsistent at consisX; simp at consisX
+  have : ∃ E ∈ endNodesOf ⟨X, LocTabX⟩, Consistent E := by
+    refine consThenConEndnode ?_ ?_; unfold Consistent Inconsistent; simp; exact consisX
+  aesop
+
+theorem InconsEndnodeThenIncons : Inconsistent X → (∃ tX : LocalTableau X, ∀ E ∈ endNodesOf ⟨X, tX⟩, Inconsistent E) := by
+  intro inconsisX; unfold Inconsistent at inconsisX; simp at inconsisX; rcases inconsisX with ⟨inconsisX⟩;
+  let inconsisX' := inconsisX
+  induction inconsisX; all_goals clear X;
+  swap
+  -- Simple case
+  rename_i X α nBoxα simpleX closTabproj IH
+  use sim simpleX; unfold endNodesOf; simp; unfold Inconsistent; simp; exact Nonempty.intro inconsisX'
+
+  -- Local-Rule case
+  rename_i X LocTabX endNodes IH
+  use LocTabX; exact fun E a => InconsThenInconsEndnode (IH E a)
+
+-- Inconsistent iff there is a localTab where every endnode is Inconsistent
+theorem InconsIffInconsEndnode : Inconsistent X ↔ (∃ tX : LocalTableau X, ∀ E ∈ endNodesOf ⟨X, tX⟩, Inconsistent E) := by
+  refine iff_def.mpr ?_; refine And.intro ?_ ?_; exact fun a => InconsEndnodeThenIncons a; exact fun a => InconsThenInconsEndnode a
+
+
+
 
 -- Consistent node means every LocalTableau has a endNode whose projections are consistent
 theorem consThenConProjEndNode : Consistent X → ∀ tX : LocalTableau X, ∃ E ∈ endNodesOf ⟨X, tX⟩, ∀ α, ~ (□ α) ∈ E → Consistent (projection E ∪ {~α})  := by
@@ -73,3 +104,51 @@ theorem consThenConProjEndNode : Consistent X → ∀ tX : LocalTableau X, ∃ E
   use E; refine And.intro EEndnode ?_;
   have simpleE : Simple E := endNodeSimple EEndnode
   intro α negBoxα; exact consisImpliesProj consisE simpleE α negBoxα
+
+
+-- Consistext X and LocalRule X B  ⇒ ∃ Y,  Y ∈ B ∧ Consistent Y
+theorem conThenConSucc : Consistent X → LocalRule X B →  ∃ Y ∈ B, Consistent Y := by
+  intro consX locRule; revert consX; contrapose; simp
+  intro InconSucc; cases locRule
+  · case bot bot_in_X =>
+    unfold Consistent Inconsistent; simp; refine Nonempty.intro ?_
+    refine ClosedTableau.loc ?_ ?_; refine @LocalTableau.byLocalRule X ∅ (LocalRule.bot bot_in_X) ?_;
+    intro Y; intro YInEmpty; refine False.elim ?_; aesop
+    intro Y YEndNode; simp at YEndNode
+
+  · case Not α nα =>
+    unfold Consistent Inconsistent; simp; refine Nonempty.intro ?_
+    refine ClosedTableau.loc ?_ ?_; refine @LocalTableau.byLocalRule X ∅ (@LocalRule.Not X α nα) ?_;
+    intro Y YInEmpty; simp at YInEmpty
+
+    intro Y; intro YEndNode; simp at YEndNode
+
+  · case neg α nnα =>
+    have : Inconsistent (X \ {~~α} ∪ {α}) := by
+      unfold Inconsistent; simp; unfold Consistent Inconsistent at InconSucc; simp at InconSucc; exact InconSucc
+    clear InconSucc; rename (Inconsistent (X \ {~~α} ∪ {α})) => InconSucc
+    have :  (∃ tXα : LocalTableau (X \ {~~α} ∪ {α}), ∀ E ∈ endNodesOf ⟨(X \ {~~α} ∪ {α}), tXα⟩, Inconsistent E) := InconsIffInconsEndnode.mp InconSucc
+    clear InconSucc; rename (∃ tXα : LocalTableau (X \ {~~α} ∪ {α}), ∀ E ∈ endNodesOf ⟨(X \ {~~α} ∪ {α}), tXα⟩, Inconsistent E) => InconSucc
+    rcases InconSucc with ⟨tXα, endNodeincon⟩
+    suffices : Inconsistent X; exact InconsisIffNotConsis.mp this
+    suffices : (∃ tX : LocalTableau X, ∀ E ∈ endNodesOf ⟨X, tX⟩, Inconsistent E); exact InconsIffInconsEndnode.mpr this
+    by_contra h₀; simp at h₀
+    have : ∀ Z, ∀ eq : (X \ {~~α} ∪ {α} = Z), ∀ E ∈ endNodesOf ⟨Z, Eq.rec tXα eq⟩, Inconsistent E := by
+      intro Z eq; subst Z; exact fun E a => endNodeincon E a
+    clear endNodeincon; rename (∀ Z, ∀ eq : (X \ {~~α} ∪ {α} = Z), ∀ E ∈ endNodesOf ⟨Z, Eq.rec tXα eq⟩, Inconsistent E) => endNodeincon
+    let LocTabX : LocalTableau X := by
+      refine @LocalTableau.byLocalRule X {X \ {~~α} ∪ {α}} ?_ ?_
+      exact LocalRule.neg nnα
+      intro Y Yeq;
+      have eq : X \ {~~α} ∪ {α} = Y := by
+        simp at *; exact id Yeq.symm
+      exact Eq.rec tXα eq
+    specialize h₀ LocTabX; rcases h₀ with ⟨E, EEnode, consisE⟩
+    unfold endNodesOf at EEnode; simp at EEnode; dsimp at EEnode
+    sorry
+
+  · case Con α β α_β =>
+    sorry
+
+  · case nCo α β nα_β =>
+    sorry
