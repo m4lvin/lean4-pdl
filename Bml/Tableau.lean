adc17af065f5c9e0947c1f761002e9d1f8e97d0c
18jesorkin <18jesorkin@gmail.com>
2024-01-27 13:31:44 -0500

    Fixed LocalRule and LocalTableau

--- Bml/Tableau.lean
+++ Bml/Tableau.lean
@@ -107,33 +107,33 @@ theorem projection_set_length_leq : ∀ X, lengthOfSet (projection X) ≤ length
         _ ≤ lengthOfFormula f + S.sum lengthOfFormula := by simp; apply IH
 
 -- local rules: given this pair, we get these pairs as child nodes
-inductive LocalRule : Finset Formula × Finset Formula → Finset (Finset Formula) × Finset (Finset Formula) → Type
+inductive LocalRule : Finset Formula × Finset Formula → Finset ((Finset Formula) × (Finset Formula)) → Type
   -- bot rules:
-  | botL {X Y} (h : ⊥ ∈ X) : LocalRule ⟨X,Y⟩ ⟨∅,∅⟩
-  | botR {X Y} (h : ⊥ ∈ Y) : LocalRule ⟨X,Y⟩ ⟨∅,∅⟩
+  | botL {X Y} (h : ⊥ ∈ X) : LocalRule ⟨X,Y⟩ ∅
+  | botR {X Y} (h : ⊥ ∈ Y) : LocalRule ⟨X,Y⟩ ∅
 
   -- Not rules
-  | NotL {X Y ϕ} (h : ϕ ∈ X ∧ ~ϕ ∈ X) : LocalRule ⟨X,Y⟩ ⟨∅,∅⟩
-  | NotR {X Y ϕ} (h : ϕ ∈ Y ∧ ~ϕ ∈ Y) : LocalRule ⟨X,Y⟩ ⟨∅,∅⟩
-  | NotXY {X Y ϕ} (h : ϕ ∈ X ∧ ~ϕ ∈ Y) : LocalRule ⟨X,Y⟩ ⟨∅,∅⟩
-  | NotYX {X Y ϕ} (h : ϕ ∈ Y ∧ ~ϕ ∈ X) : LocalRule ⟨X,Y⟩ ⟨∅,∅⟩
+  | NotL {X Y ϕ} (h : ϕ ∈ X ∧ ~ϕ ∈ X) : LocalRule ⟨X,Y⟩ ∅
+  | NotR {X Y ϕ} (h : ϕ ∈ Y ∧ ~ϕ ∈ Y) : LocalRule ⟨X,Y⟩ ∅
+  | NotXY {X Y ϕ} (h : ϕ ∈ X ∧ ~ϕ ∈ Y) : LocalRule ⟨X,Y⟩ ∅
+  | NotYX {X Y ϕ} (h : ϕ ∈ Y ∧ ~ϕ ∈ X) : LocalRule ⟨X,Y⟩ ∅
 
   -- neg rules:
-  | negL {X Y ϕ} (h : ~~ϕ ∈ X) : LocalRule ⟨X,Y⟩ ⟨{X \ {~~ϕ} ∪ {ϕ}}, {Y}⟩
-  | negR {X Y ϕ} (h : ~~ϕ ∈ Y) : LocalRule ⟨X,Y⟩ ⟨{X}, {Y \ {~~ϕ} ∪ {ϕ}}⟩
+  | negL {X Y ϕ} (h : ~~ϕ ∈ X) : LocalRule ⟨X,Y⟩ {⟨X \ {~~ϕ} ∪ {ϕ}, Y⟩}
+  | negR {X Y ϕ} (h : ~~ϕ ∈ Y) : LocalRule ⟨X,Y⟩ {⟨X, Y \ {~~ϕ} ∪ {ϕ}⟩}
 
   -- Con rules:
-  | ConL {X Y ϕ ψ} (h : ϕ⋀ψ ∈ X) : LocalRule ⟨X, Y⟩ ⟨{X \ {ϕ⋀ψ} ∪ {ϕ, ψ}}, {Y}⟩
-  | ConR {X Y ϕ ψ} (h : ϕ⋀ψ ∈ Y) : LocalRule ⟨X, Y⟩ ⟨{X}, {Y \ {ϕ⋀ψ} ∪ {ϕ, ψ}}⟩
+  | ConL {X Y ϕ ψ} (h : ϕ⋀ψ ∈ X) : LocalRule ⟨X, Y⟩ {⟨X \ {ϕ⋀ψ} ∪ {ϕ, ψ}, Y⟩}
+  | ConR {X Y ϕ ψ} (h : ϕ⋀ψ ∈ Y) : LocalRule ⟨X, Y⟩ {⟨X, Y \ {ϕ⋀ψ} ∪ {ϕ, ψ}⟩}
 
   -- nCo rules:
-  | nCoL {X Y ϕ ψ} (h : ~(ϕ⋀ψ) ∈ X) : LocalRule ⟨X, Y⟩ ⟨{X \ {~(ϕ⋀ψ)} ∪ {~ϕ}, X \ {~(ϕ⋀ψ)} ∪ {~ψ}}, {Y}⟩
-  | nCoR {X Y ϕ ψ} (h : ~(ϕ⋀ψ) ∈ Y) : LocalRule ⟨X, Y⟩ ⟨{X}, {Y \ {~(ϕ⋀ψ)} ∪ {~ϕ}, Y \ {~(ϕ⋀ψ)} ∪ {~ψ}}⟩
+  | nCoL {X Y ϕ ψ} (h : ~(ϕ⋀ψ) ∈ X) : LocalRule ⟨X, Y⟩ {⟨X \ {~(ϕ⋀ψ)} ∪ {~ϕ}, Y⟩, ⟨X \ {~(ϕ⋀ψ)} ∪ {~ψ}, Y⟩}
+  | nCoR {X Y ϕ ψ} (h : ~(ϕ⋀ψ) ∈ Y) : LocalRule ⟨X, Y⟩ {⟨X, Y \ {~(ϕ⋀ψ)} ∪ {~ϕ}⟩, ⟨X, Y \ {~(ϕ⋀ψ)} ∪ {~ψ}⟩}
 
 
 -- If X1 is not simple, then a local rule can be applied.
 -- (page 13)
-theorem notSimpleThenLocalRuleL {X1 X2} : ¬Simple X1 → ∃ B1 B2, Nonempty (LocalRule ⟨X1, X2⟩ ⟨B1, B2⟩) :=
+theorem notSimpleThenLocalRuleL {X1 X2} : ¬Simple X1 → ∃ B, Nonempty (LocalRule ⟨X1, X2⟩ B) :=
   by
   intro notSimple; unfold Simple at notSimple ; simp at notSimple; rcases notSimple with ⟨ϕ, ϕ_in_X1, ϕ_not_simple⟩
   cases ϕ
@@ -144,20 +144,20 @@ theorem notSimpleThenLocalRuleL {X1 X2} : ¬Simple X1 → ∃ B1 B2, Nonempty (L
     case bottom => tauto
     case atom_prop => tauto
     case neg ψ =>
-      use{X1 \ {~~ψ} ∪ {ψ}}; use {X2}
+      use {⟨X1 \ {~~ψ} ∪ {ψ}, X2⟩};
       use LocalRule.negL ϕ_in_X1
     case And ψ1 ψ2 =>
-      use{X1 \ {~(ψ1⋀ψ2)} ∪ {~ψ1}, X1 \ {~(ψ1⋀ψ2)} ∪ {~ψ2}}; use {X2}
+      use {⟨X1 \ {~(ψ1⋀ψ2)} ∪ {~ψ1}, X2⟩, ⟨X1 \ {~(ψ1⋀ψ2)} ∪ {~ψ2}, X2⟩};
       use LocalRule.nCoL ϕ_in_X1
     case box => tauto
   case And ψ1 ψ2 =>
-    use{X1 \ {ψ1⋀ψ2} ∪ {ψ1, ψ2}}; use {X2}
+    use{⟨X1 \ {ψ1⋀ψ2} ∪ {ψ1, ψ2}, X2⟩};
     use LocalRule.ConL ϕ_in_X1
   case box => tauto
 
 -- If X2 is not simple, then a local rule can be applied.
 -- (page 13)
-theorem notSimpleThenLocalRuleR {X1 X2} : ¬Simple X2 → ∃ B1 B2, Nonempty (LocalRule ⟨X1, X2⟩ ⟨B1, B2⟩) :=
+theorem notSimpleThenLocalRuleR {X1 X2} : ¬Simple X2 → ∃ B, Nonempty (LocalRule ⟨X1, X2⟩ B) :=
   by
   intro notSimple; unfold Simple at notSimple ; simp at notSimple; rcases notSimple with ⟨ϕ, ϕ_in_X2, ϕ_not_simple⟩
   cases ϕ
@@ -168,21 +168,21 @@ theorem notSimpleThenLocalRuleR {X1 X2} : ¬Simple X2 → ∃ B1 B2, Nonempty (L
     case bottom => tauto
     case atom_prop => tauto
     case neg ψ =>
-      use {X1}; use{X2 \ {~~ψ} ∪ {ψ}};
+      use {⟨X1, X2 \ {~~ψ} ∪ {ψ}⟩};
       use LocalRule.negR ϕ_in_X2
     case And ψ1 ψ2 =>
-      use {X1}; use{X2 \ {~(ψ1⋀ψ2)} ∪ {~ψ1}, X2 \ {~(ψ1⋀ψ2)} ∪ {~ψ2}};
+      use {⟨X1, X2 \ {~(ψ1⋀ψ2)} ∪ {~ψ1}⟩, ⟨X1, X2 \ {~(ψ1⋀ψ2)} ∪ {~ψ2}⟩};
       use LocalRule.nCoR ϕ_in_X2
     case box => tauto
   case And ψ1 ψ2 =>
-    use {X1}; use{X2 \ {ψ1⋀ψ2} ∪ {ψ1, ψ2}};
+    use {⟨X1, X2 \ {ψ1⋀ψ2} ∪ {ψ1, ψ2}⟩};
     use LocalRule.ConR ϕ_in_X2
   case box => tauto
 
 
 
 
-theorem localRulesDecreaseLengthL (r : LocalRule ⟨X1, X2⟩ ⟨B1, B2⟩) : ∀ Y1 ∈ B1, lengthOfSet Y1 < lengthOfSet X1 ∨ ∀ Y2 ∈ B2, lengthOfSet Y2 < lengthOfSet X2 :=
+theorem localRulesDecreaseLengthL (r : LocalRule ⟨X1, X2⟩ B) : ∀ pair ∈ B, lengthOfPair pair < lengthOfPair ⟨X1, X2⟩ :=
   by
   cases r
   all_goals intro β inB; simp at *
@@ -253,7 +253,7 @@ theorem atmRuleDecreasesLength {X : Finset Formula} {ϕ} :
 -- mixed with Definition 11 (with all PDL stuff missing for now)
 -- a local tableau for X, must be maximal
 inductive LocalTableau : Finset Formula × Finset Formula → Type
-  | byLocalRule {X1 X2 B1 B2} (_ : LocalRule ⟨X1, X2⟩ ⟨B1, B2⟩) (next : ∀ Y1 ∈ B1, ∀ Y2 ∈ B2, LocalTableau ⟨Y1, Y2⟩) : LocalTableau ⟨X1, X2⟩
+  | byLocalRule {X1 X2 B} (_ : LocalRule ⟨X1, X2⟩ B) (next : ∀ pair ∈ B, LocalTableau pair) : LocalTableau ⟨X1, X2⟩
   | sim {X1 X2} : Simple (X1) → Simple (X2) → LocalTableau ⟨X1, X2⟩
 
 
