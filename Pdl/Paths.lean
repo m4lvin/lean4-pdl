e17deaaf7b631c618d46913a0b6800e0e97e211e
Malvin Gattinger <malvin@w4eg.de>
2024-05-24 23:22:26 +0200

    rewrite `witness`` as inductive type; try to prove `existsWitnessPath``

--- Pdl/Paths.lean
+++ Pdl/Paths.lean
@@ -9,67 +9,49 @@ abbrev WorldPath {W} (M : KripkeModel W) := Subtype $ List.Chain' (atomRel M)
 def WorldPath.tail (π : WorldPath M) : WorldPath M := ⟨π.1.tail, π.2.tail⟩
 
 /-- Does path `π` starting at v0 witness `¬□(δ,ξ)`? -/
--- IDEA: rewrite this as an inductive prop instead of matching?
-def witnessing (M : KripkeModel W) (v0 : W) (π : WorldPath M) (δ : List Program) (ξ : Formula) : Prop :=
-  match π.1, δ with
-    -- Case |π| = 0:
-    | [], [] => (M,v0) ⊨ (~ξ)
-    | [], _::_ => False
-    -- Case |π| > 0:
-    | _ :: _ , [] => False
-    | v1 :: rest , α::γs =>
-        (∃ a, α = (Program.atom_prog a) ∧ witnessing M v1 π.tail γs ξ)
-      ∨ ∃ Fβ ∈ H α,
-        (M,v0) ⊨ Fβ.1
-        ∧ (
-          (Fβ.2 = ε ∧ witnessing M v0 π γs ξ)
-          ∨
-          (∃ a, ∃ ηs, Fβ.2 = ((Program.atom_prog a) :: ηs) ∧ witnessing M v1 π.tail (ηs ++ γs) ξ)
-          )
-termination_by (π.1.length, δ.length)
-decreasing_by
-  all_goals sorry
+-- NOTE: Not using `WorldPath` any more, atom relation is built in here!
+inductive witness (M : KripkeModel W) : W  → List W → List Program → Formula → Prop
+ -- Case |π| = 0:
+| nil : (M,v0) ⊨ (~ξ) → witness M v0 [] [] ξ
+ -- Case |π| > 0:
+| consAtom : M.Rel a v0 v1 → witness M v1 rest γs ξ → witness M v0 (v1 :: rest) ((·a) :: γs) ξ
+| consEmpty Fs : (⟨Fs, ε⟩ ∈ H α) → (M, v0) ⊨ Fs → witness M v0 π γs ξ → witness M v0 π (α::γs) ξ
+| consComp : M.Rel a v0 v1 → (⟨Fs, (·a : Program) :: η⟩ ∈ H α) → (M, v0) ⊨ Fs → witness M v1 rest (ηs ++ γs) ξ → witness M v0 (v1 :: rest) (α::γs) ξ
 
 theorem existsWitnessPath {M : KripkeModel W} δ v :
-    (M,v) ⊨ (~ Formula.boxes δ ξ) ↔ ∃ π, witnessing M v π δ ξ := by
+    (M,v) ⊨ (~ Formula.boxes δ ξ) ↔ ∃ π, witness M v π δ ξ := by
   induction δ generalizing v
   case nil =>
-    simp [modelCanSemImplyForm, witnessing]
+    simp [modelCanSemImplyForm, witness]
     constructor
     · intro nvXi
-      use [], (by simp)
-      simp only [witnessing, modelCanSemImplyForm, evaluatePoint, evaluate]
+      use []
+      apply witness.nil
+      simp only [modelCanSemImplyForm, evaluatePoint, evaluate]
       exact nvXi
-    · rintro ⟨π, ⟨l, foo⟩⟩
-      cases π
-      · simp_all [witnessing, modelCanSemImplyForm]
-      · exfalso; unfold witnessing at foo; tauto
+    · rintro ⟨π, wit⟩
+      cases wit
+      simp only [modelCanSemImplyForm, evaluatePoint, evaluate] at *
+      assumption
   case cons d δ IH =>
     simp only [Formula.boxes]
     constructor
-    · simp only [modelCanSemImplyForm, evaluatePoint, evaluate, not_forall, exists_prop, forall_exists_index, and_imp] at *
+    · simp [modelCanSemImplyForm, evaluatePoint, evaluate, not_forall, exists_prop, forall_exists_index, and_imp] at *
       intro w v_d_w not_w_
       rw [IH] at not_w_
       clear IH
-      rcases not_w_ with ⟨⟨π, l⟩, wit⟩
-      -- simp [witnessing] at *
-      cases d
+      rcases not_w_ with ⟨π, wit⟩
+      cases d -- Distinguish cases by the first program `d` in the path `d :: δ`.
       all_goals simp only [relate] at v_d_w
       case atom_prog a =>
-        cases π <;> cases δ -- yield four cases, matching those in `def witnessing`.
-        · refine ⟨⟨[w], ?_⟩, ?_⟩
-          simp
-          unfold witnessing
-          simp
-          left
-          assumption
-        · exfalso; simp [witnessing] at wit
-        · exfalso; unfold witnessing at wit; exact wit
-        · unfold witnessing
-          simp
-          sorry
+        exact ⟨w :: π, witness.consAtom v_d_w wit⟩
       case test φ =>
-        sorry
+        rcases v_d_w with ⟨v_is_w, v_φ⟩
+        subst v_is_w
+        use π
+        apply witness.consEmpty [φ] (by simp [H]) (by simp [modelCanSemImplyList]; exact v_φ) wit
+      -- NEXT: in all remaining cases, use witness.consComp
+      -- QUESTION: Do we need IHs/recursion now? What if δ becomes longer in the recursive call?
       case sequence α β =>
         sorry
       case union α β =>
