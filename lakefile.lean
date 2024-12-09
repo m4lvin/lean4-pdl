532b71c6c2ad57631ad2d53eb5c6e6a9b142c55c
Malvin Gattinger <malvin@w4eg.de>
2024-12-09 20:06:16 +0100

    clean up lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,17 +1,13 @@
 import Lake
 open Lake DSL
 
-package «pdl» where
-  -- add any package configuration options here
+package «pdl»
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"v4.13.0"
 
--- require "marcusrossel" / "egg" @ git "main"
-
 @[default_target]
-lean_lib «Pdl» where
-  -- add any library configuration options here
+lean_lib «Pdl»
 
-lean_lib «Bml» where
-  -- add any library configuration options here
+lean_lib «Bml»
+
