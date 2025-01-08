8040dd2af6e4eddd5a30d75c007037d8649e4f58
Malvin Gattinger <malvin@w4eg.de>
2025-01-08 14:00:26 +0100

    make FL closed under single negations etc.

--- Unused/FischerLadner.lean
+++ Unused/FischerLadner.lean
@@ -1,3 +1,4 @@
+import Mathlib.Data.Finset.Basic
 import Mathlib.Order.FixedPoints
 
 import Pdl.Syntax
@@ -8,42 +9,50 @@ import Pdl.Syntax
 NOTE: This file is currently unused!
 -/
 
-def fischerLadnerOf : Formula → Set Formula
+/-- Single step to close a single formula under:
+-- - single-negations,
+-- - subformulas, and
+-- - unraveling PDL programs. -/
+def flOf : Formula → Finset Formula
 | ⊥ => {}
-| ·a => {}
+| ·_ => {}
 | φ⋀ψ => {~(φ⋀ψ), φ, ψ}
-| ⌈·_⌉φ => {φ}
-| ⌈α;'β⌉φ => { ⌈α⌉⌈β⌉φ }
-| ⌈α⋓β⌉φ => { ⌈α⌉φ, ⌈β⌉φ }
-| ⌈∗α⌉φ => { φ, ⌈α⌉⌈∗α⌉φ }
-| ⌈?'τ⌉φ => { τ, φ }
-| ~~φ => {~φ}
-| ~⊥ => {}
-| ~(·_) => {}
-| ~(φ⋀ψ) => {φ⋀ψ}
-| ~⌈·_⌉φ => {~φ}
-| ~⌈α;'β⌉φ => { ~⌈α⌉⌈β⌉φ }
-| ~⌈α⋓β⌉φ => { ~⌈α⌉φ, ~⌈β⌉φ }
-| ~⌈∗α⌉φ => { ~φ, ~⌈α⌉⌈∗α⌉φ }
-| ~⌈?'τ⌉φ => { ~τ, ~φ }
+| ⌈·a⌉φ => { ~⌈·a⌉φ, φ }
+| ⌈α;'β⌉φ => { ~⌈α;'β⌉φ, ⌈α⌉⌈β⌉φ }
+| ⌈α⋓β⌉φ => { ~⌈α⋓β⌉φ, ⌈α⌉φ, ⌈β⌉φ }
+| ⌈∗α⌉φ => { ~⌈∗α⌉φ, φ, ⌈α⌉⌈∗α⌉φ }
+| ⌈?'τ⌉φ => { ~⌈?'τ⌉φ, τ, φ }
+| ~~φ => { ~φ }
+| ~⊥ => { ⊥ }
+| ~(·p) => { (·p : Formula) }
+| ~(φ⋀ψ) => { φ⋀ψ }
+| ~⌈·a⌉φ => { ⌈·a⌉φ, ~φ }
+| ~⌈α;'β⌉φ => { ⌈α;'β⌉φ, ~⌈α⌉⌈β⌉φ }
+| ~⌈α⋓β⌉φ => { ⌈α⋓β⌉φ, ~⌈α⌉φ, ~⌈β⌉φ }
+| ~⌈∗α⌉φ => { ⌈∗α⌉φ, ~φ, ~⌈α⌉⌈∗α⌉φ }
+| ~⌈?'τ⌉φ => { ⌈?'τ⌉φ, ~τ, ~φ }
 
-def fischerLadnerStep : Set Formula  → Set Formula
-| X => X ∪ (⋃ φ ∈ X, fischerLadnerOf φ)
+/-- Single step for the closure of a set of formulas. -/
+def flStep : Set Formula  → Set Formula
+| X => X ∪ (⋃ φ ∈ X, flOf φ)
 
-theorem flS_isMonotone : Monotone fischerLadnerStep := by
-  sorry
+theorem flStep_isMonotone : Monotone flStep := by -- Thanks Andrés!
+  intro X Y h
+  simp_all only [Set.le_eq_subset, flStep]
+  intro x hx
+  cases hx <;> aesop
 
-def flHom : OrderHom (Set Formula) (Set Formula) := ⟨fischerLadnerStep, flS_isMonotone⟩
+def flHom : OrderHom (Set Formula) (Set Formula) := ⟨flStep, flStep_isMonotone⟩
 
--- I think `lfp` is not what I want, because it does not ask me for a strarting value.
--- Probably the lfp of flHom in general is just {} ;-)
-def fischerLadner_NOPE : Set Formula := OrderHom.lfp flHom
-
-theorem X_le_flX : X ≤ flHom X := by
+theorem le_flHom : X ≤ flHom X := by
   intro f f_in
-  simp [flHom, fischerLadnerStep]
+  simp [flHom, flStep]
   left
   assumption
 
--- Apparently I want `nextFixed` instead of `lfp`?
-def fischerLadner (X : Set Formula) : Set Formula := OrderHom.nextFixed flHom X X_le_flX
+/-- The Fischer-Ladner closure defined as a fixed point of `flStep`.
+Note that we use `nextFixed` and not `lfp`. -/
+def fischerLadner (X : Set Formula) : Set Formula := OrderHom.nextFixed flHom X le_flHom
+
+-- theorem fischerLadner_finite : (fischerLadner X).Finite := by
+--   sorry
