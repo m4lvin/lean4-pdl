5ca3508ea8d832640e1fb2cff9ce07d3ab22cdc8
Malvin Gattinger <malvin@w4eg.de>
2025-01-08 13:58:23 +0100

    alternative def of FL() via OrderHom.nextFixed

--- Unused/FischerLadner.lean
+++ Unused/FischerLadner.lean
@@ -1,35 +1,49 @@
+import Mathlib.Order.FixedPoints
+
 import Pdl.Syntax
 
--- NOTE: This file is currently unused!
+/-!
+# Fischer-Ladner Closure
 
-/-! ## Fischer-Ladner Closure -/
-
-def fischerLadnerStep : Formula → List Formula
-| ⊥ => []
-| ·_ => []
-| φ⋀ψ => [~(φ⋀ψ), φ, ψ]
-| ⌈·_⌉φ => [φ]
-| ⌈α;'β⌉φ => [ ⌈α⌉⌈β⌉φ ]
-| ⌈α⋓β⌉φ => [ ⌈α⌉φ, ⌈β⌉φ ]
-| ⌈∗α⌉φ => [ φ, ⌈α⌉⌈∗α⌉φ ]
-| ⌈?'τ⌉φ => [ τ, φ ]
-| ~~φ => [~φ]
-| ~⊥ => []
-| ~(·_) => []
-| ~(φ⋀ψ) => [φ⋀ψ]
-| ~⌈·_⌉φ => [~φ]
-| ~⌈α;'β⌉φ => [ ~⌈α⌉⌈β⌉φ ]
-| ~⌈α⋓β⌉φ => [ ~⌈α⌉φ, ~⌈β⌉φ ]
-| ~⌈∗α⌉φ => [ ~φ, ~⌈α⌉⌈∗α⌉φ ]
-| ~⌈?'τ⌉φ => [ ~τ, ~φ ]
-
-/-
-def fischerLadner : List Formula → List Formula
-| X => let Y := X ∪ (X.map fischerLadnerStep).join
-       if Y ⊆ X then Y else fischerLadner Y
-decreasing_by sorry -- hmm??
+NOTE: This file is currently unused!
 -/
 
--- Examples:
--- #eval fischerLadner [~~((·1) : Formula)]
--- #eval fischerLadner [ ((⌈(·1) ⋓ (·2)⌉(·3)) ⟷ ((⌈(·1)⌉(·3)) ⋀ (⌈(·2)⌉(·3)))) ]
+def fischerLadnerOf : Formula → Set Formula
+| ⊥ => {}
+| ·a => {}
+| φ⋀ψ => {~(φ⋀ψ), φ, ψ}
+| ⌈·_⌉φ => {φ}
+| ⌈α;'β⌉φ => { ⌈α⌉⌈β⌉φ }
+| ⌈α⋓β⌉φ => { ⌈α⌉φ, ⌈β⌉φ }
+| ⌈∗α⌉φ => { φ, ⌈α⌉⌈∗α⌉φ }
+| ⌈?'τ⌉φ => { τ, φ }
+| ~~φ => {~φ}
+| ~⊥ => {}
+| ~(·_) => {}
+| ~(φ⋀ψ) => {φ⋀ψ}
+| ~⌈·_⌉φ => {~φ}
+| ~⌈α;'β⌉φ => { ~⌈α⌉⌈β⌉φ }
+| ~⌈α⋓β⌉φ => { ~⌈α⌉φ, ~⌈β⌉φ }
+| ~⌈∗α⌉φ => { ~φ, ~⌈α⌉⌈∗α⌉φ }
+| ~⌈?'τ⌉φ => { ~τ, ~φ }
+
+def fischerLadnerStep : Set Formula  → Set Formula
+| X => X ∪ (⋃ φ ∈ X, fischerLadnerOf φ)
+
+theorem flS_isMonotone : Monotone fischerLadnerStep := by
+  sorry
+
+def flHom : OrderHom (Set Formula) (Set Formula) := ⟨fischerLadnerStep, flS_isMonotone⟩
+
+-- I think `lfp` is not what I want, because it does not ask me for a strarting value.
+-- Probably the lfp of flHom in general is just {} ;-)
+def fischerLadner_NOPE : Set Formula := OrderHom.lfp flHom
+
+theorem X_le_flX : X ≤ flHom X := by
+  intro f f_in
+  simp [flHom, fischerLadnerStep]
+  left
+  assumption
+
+-- Apparently I want `nextFixed` instead of `lfp`?
+def fischerLadner (X : Set Formula) : Set Formula := OrderHom.nextFixed flHom X X_le_flX
